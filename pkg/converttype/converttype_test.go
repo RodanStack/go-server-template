488b52b2f16c9d702ad974864bd11f32c8509858
Rodan Ramdam <rodan.ramdam@gmail.com>
2024-08-06 13:43:17 +0545

    refactor: pkg converttype and tests

--- pkg/converttype/converttype_test.go
+++ pkg/converttype/converttype_test.go
@@ -5,34 +5,29 @@ import (
 	"testing"
 )
 
-func TestStringToUint_NonEmptyString(t *testing.T) {
-	str := "123"
-	expected := uint(123)
-	num, err := converttype.StringToUint(str)
-	if err != nil {
-		t.Errorf("Expected nil error, got %v", err)
+func TestStringToUint(t *testing.T) {
+	tests := []struct {
+		name     string
+		str      string
+		expected uint
+		wantErr  bool
+	}{
+		{"NonEmptyString", "123", 123, false},
+		{"InvalidString", "abc", 0, true},
+		{"EmptyString", "", 0, false},
+		{"NegativeNumberString", "-123", 0, true},
+		{"OverflowUint", "18446744073709551616", 0, true}, // Value larger than max int
 	}
-	if num != expected {
-		t.Errorf("Expected %v, got %v", expected, num)
-	}
-}
 
-func TestStringToUint_InvalidString(t *testing.T) {
-	str := "abc"
-	_, err := converttype.StringToUint(str)
-	if err == nil {
-		t.Errorf("Expected non-nil error, got nil")
-	}
-}
-
-func TestStringToUint_MaxUint(t *testing.T) {
-	str := "4294967295"
-	expected := uint(4294967295)
-	num, err := converttype.StringToUint(str)
-	if err != nil {
-		t.Errorf("Expected nil error, got %v", err)
-	}
-	if num != expected {
-		t.Errorf("Expected %v, got %v", expected, num)
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			num, err := converttype.StringToUint(tt.str)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("Expected error: %v, got: %v", tt.wantErr, err)
+			}
+			if num != tt.expected {
+				t.Errorf("Expected %v, got %v", tt.expected, num)
+			}
+		})
 	}
 }
--- pkg/converttype/string.go
+++ pkg/converttype/string.go
@@ -1,10 +1,13 @@
 package converttype
 
-import "strconv"
+import (
+	"errors"
+	"strconv"
+)
 
 // StringToUint converts a string to a uint.
 // If the string is empty, it returns 0.
-// If the string is not a valid number, it returns an error.
+// If the string is not a valid number or is negative, it returns an error.
 func StringToUint(str string) (uint, error) {
 	if str == "" {
 		return 0, nil
@@ -13,6 +16,10 @@ func StringToUint(str string) (uint, error) {
 	parsedInt, err := strconv.Atoi(str)
 	if err != nil {
 		return 0, err
+	}
+
+	if parsedInt < 0 {
+		return 0, errors.New("negative value cannot be converted to uint")
 	}
 
 	return uint(parsedInt), nil
