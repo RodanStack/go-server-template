8b127392d6ba1b7948e548587b76f844bea25f5b
Rodan Ramdam <rodan.ramdam@gmail.com>
2024-08-06 11:00:32 +0545

    fix: line is 152 characters (lll) error

--- pkg/sliceutil/sliceutil.go
+++ pkg/sliceutil/sliceutil.go
@@ -30,6 +30,8 @@ func Contains[T comparable](slice []T, val T) bool {
 //		inRange := IsBetween(value, min, max)
 //		fmt.Println(inRange) // Output: true
 //	}
-func IsBetween[T ~int | ~int8 | ~int16 | ~int32 | ~int64 | ~uint | ~uint8 | ~uint16 | ~uint32 | ~uint64 | ~float32 | ~float64](value, min, max T) bool {
+func IsBetween[
+	T ~int | ~int8 | ~int16 | ~int32 | ~int64 | ~uint | ~uint8 | ~uint16 | ~uint32 | ~uint64 | ~float32 | ~float64,
+](value, min, max T) bool {
 	return value >= min && value <= max
 }
