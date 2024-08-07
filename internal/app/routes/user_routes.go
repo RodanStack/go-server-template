9957c0d16a6d5ec2d0bd35eb97ea3267707fb82a
Rodan Ramdam <rodan.ramdam@gmail.com>
2024-08-07 21:45:13 +0545

    feat: setup get user routes

--- internal/app/routes/user_routes.go
+++ internal/app/routes/user_routes.go
@@ -1,12 +1,32 @@
 package routes
 
+import (
+	"go-server-template/internal/app/controllers"
+	"go-server-template/internal/infra/router"
+
+	"go.uber.org/fx"
+)
+
 type UserRoutes struct {
+	router     *router.Router
+	controller *controllers.UserController
 }
 
-func NewUserRoutes() *UserRoutes {
-	return &UserRoutes{}
+type userRoutesParams struct {
+	fx.In
+
+	Router     *router.Router
+	Controller *controllers.UserController
+}
+
+func NewUserRoutes(params userRoutesParams) *UserRoutes {
+	return &UserRoutes{params.Router, params.Controller}
 }
 
 func (u *UserRoutes) RegisterRoutes() {
 	// Register routes here
+
+	routes := u.router.Group("/api/v1/users")
+
+	routes.GET("/", u.controller.GetUsers)
 }
