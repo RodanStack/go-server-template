4b0b8ce2d35fde6f92a5323f295e80f4f9bb187b
Rodan Ramdam <rodan.ramdam@gmail.com>
2024-08-08 22:28:55 +0545

    feat: setup release mode based on the environment for gin router

--- internal/infra/router/router.go
+++ internal/infra/router/router.go
@@ -2,6 +2,7 @@ package router
 
 import (
 	"go-server-template/internal/pkg/apires"
+	"go-server-template/pkg/config"
 
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
@@ -14,9 +15,22 @@ type Router struct {
 
 type Dependencies struct {
 	fx.In
+
+	Env *config.Env
 }
 
-func NewRouter(_ Dependencies) *Router {
+func NewRouter(deps Dependencies) *Router {
+	environment := deps.Env.Environment
+
+	switch environment {
+	case "production":
+		gin.SetMode(gin.ReleaseMode)
+	case "development":
+		gin.SetMode(gin.DebugMode)
+	default:
+		gin.SetMode(gin.DebugMode)
+	}
+
 	router := gin.Default()
 
 	const maxMultipartMemory = 10 << 20 // 10 MiB
