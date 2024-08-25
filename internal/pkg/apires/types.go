caa5396e342c0673b15a2af2b88d6edce82038bb
Rodan Ramdam <rodan.ramdam@gmail.com>
2024-08-25 19:50:02 +0545

    feat: use status as explicit type of string

--- internal/pkg/apires/types.go
+++ internal/pkg/apires/types.go
@@ -1,21 +1,23 @@
 package apires
 
+type Status string
+
+const (
+	StatusSuccess Status = "success"
+	StatusError   Status = "error"
+)
+
 type Response struct {
-	Status  string      `json:"status"`
-	Message string      `json:"message"`
+	Status  Status      `json:"status"`
+	Message string      `json:"message,omitempty"`
 	Data    interface{} `json:"data,omitempty"`
 	Meta    interface{} `json:"meta,omitempty"` // Additional metadata
 }
 
 type ErrorResponse struct {
-	Status  string            `json:"status"`
+	Status  Status            `json:"status"`
 	Message string            `json:"message"`
 	Error   string            `json:"error,omitempty"`
 	Fields  map[string]string `json:"fields,omitempty"`   // For validation errors
 	TraceID string            `json:"trace_id,omitempty"` // For tracing errors
 }
-
-const (
-	StatusSuccess = "success"
-	StatusError   = "error"
-)
