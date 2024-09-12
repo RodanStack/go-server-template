06c71fa0bce151fdf3582999170f68157b3ded39
Rodan Ramdam <rodan.ramdam@gmail.com>
2024-09-12 22:20:23 +0545

    fix: use dynamic db host and port

--- internal/infrastructure/persistence/database/database.go
+++ internal/infrastructure/persistence/database/database.go
@@ -5,13 +5,15 @@ import (
 	"fmt"
 	"go-server-template/pkg/config"
 	"log"
+	"net"
 	"time"
 
 	"github.com/jackc/pgx/v5/pgxpool"
 )
 
 func NewDatabase(env *config.Env) *pgxpool.Pool {
-	dbURL := fmt.Sprintf("postgres://%s:%s@localhost:5432/%s?sslmode=disable", env.DBUser, env.DBPassword, env.DBName)
+	dbURL := fmt.Sprintf("postgres://%s:%s@%s",
+		env.DBUser, env.DBPassword, net.JoinHostPort(env.DBHost, env.DBPort)) + fmt.Sprintf("/%s?sslmode=disable", env.DBName)
 	config, err := pgxpool.ParseConfig(dbURL)
 	if err != nil {
 		log.Fatalf("Unable to parse DATABASE_URL: %v\n", err)
